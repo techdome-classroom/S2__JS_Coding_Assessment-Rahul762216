80011fd05c5a8d6293db27f1e787650768968c0f
Rahul762216 <rahulkumar004579@gmail.com>
2024-09-25 14:52:51 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,31 +1,30 @@
 function romanToInt(s) {
-    // Map of Roman numerals to their corresponding integer values
-    const romanMap = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    };
+  const romanMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
 
-    let total = 0;  
-    let prevValue = 0;  
-    for (let i = 0; i < s.length; i++) {
-        const currentValue = romanMap[s[i]];  
-        if (currentValue > prevValue) {
-            total += currentValue - 2 * prevValue;
-        } else {
-            total += currentValue;  
-        }
- 
-        prevValue = currentValue;
+  let total = 0;
+  let prevValue = 0;
+  for (let i = 0; i < s.length; i++) {
+    const currentValue = romanMap[s[i]];
+    if (currentValue > prevValue) {
+      total += currentValue - 2 * prevValue;
+    } else {
+      total += currentValue;
     }
 
-    return total;
+    prevValue = currentValue;
+  }
+
+  return total;
 }
- 
-console.log(romanToInt("III"));  
-console.log(romanToInt("LVIII"));  
-console.log(romanToInt("MCMXCIV"));  
+
+console.log(romanToInt("III"));
+console.log(romanToInt("LVIII"));
+console.log(romanToInt("MCMXCIV"));
